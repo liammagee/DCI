889a9fd413cde1ba47a848ab3b4b52e7031f71af
Anjs04 <anjali.s2104@gmail.com>
2015-11-17 15:54:08 +1100

    Sample data
    Includes age and gender variables

---
+++ R/Generating sample data.R
@@ -0,0 +1,9 @@
+a = 12:65
+age = sample(a, 2000, replace=T)
+
+g = c("Male", "Female", "Other (specify)", "Refused")
+gender = sample(g, 2000, replace=T, prob = c(49, 49, 1, 1))
+
+dci = data.frame(age, gender)
+
+write.csv(dci, "dci.csv")
